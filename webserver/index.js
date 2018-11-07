a6eb74ff1b7476b83257e2a67be15247b9a40822
adam <atomicpenguines@gmail.com>
2018-11-07 07:34:20 +0000

    node version of server

---
+++ webserver/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("hello i am a webserver");
+});
+
+app.get("/time", (req, res) => {
+  const time = new Date().toString();
+  return res.send(`The time is: ${time}`);
+});
+
+app.get("/hello", (req, res) => {
+  if (!req.query.name) {
+    res.send(400).end();
+  }
+  return res.send(`Hello: ${req.query.name}`);
+});
+
+app.get("/user/:name", (req, res) => {
+  return res.send(`Userprofile of ${req.params.name}`);
+});
+
+app.listen(3000);
