0582574b7919fe0d682bf5f6102ce8bd5392d505
adam <atomicpenguines@gmail.com>
2018-11-07 06:23:19 +0000

    inital commit

---
+++ webserver/pureNodeServer.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const url = require("url");
+
+function handler(req, res) {
+  const parsedUrl = url.parse(req.url, true);
+  console.log(parsedUrl);
+
+  if (parsedUrl.pathname === "/") {
+    res.writeHead(200, { "Content-type": "text/plain" });
+    res.write("Hello I am a webserver");
+    return res.end();
+  } else if (parsedUrl.pathname === "/time") {
+    res.writeHead(200, { "Content-type": "text/plain" });
+    res.write(new Date().toString());
+    return res.end();
+  } else if (parsedUrl.pathname === "/hello") {
+    res.writeHead(200, { "Content-type": "text/plain" });
+    let name = parsedUrl.query.name;
+    res.write(`Hello ${name}`);
+    return res.end();
+  } else {
+    res.writeHead(404, { "Content-type": "text/plain" });
+    return res.end();
+  }
+}
+
+const server = http.createServer(handler);
+
+server.listen(3000);
