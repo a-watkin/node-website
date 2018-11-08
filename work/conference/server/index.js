43925f3678c653d4a2b870b4e4d35712c625de82
adam <atomicpenguines@gmail.com>
2018-11-08 07:42:24 +0000

    added static assets and middleware to serve them, also added favicon route

--- work/conference/server/index.js
+++ work/conference/server/index.js
@@ -4,6 +4,14 @@ const app = express();
 // defaults to index.js
 const routes = require("./routes");
 
+// always looks in the root of the project so it find public there
+// and automatically makes the files there available
+app.use(express.static("public"));
+
+app.get("/favicon.ico", (req, res, next) => {
+  return res.sendStatus(204);
+});
+
 // routing middleware
 app.use("/", routes());
 
