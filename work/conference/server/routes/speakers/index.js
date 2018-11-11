16e6a1e55565eb5612e8942f580afae4539cb1fe
adam <atomicpenguines@gmail.com>
2018-11-11 02:30:15 +0000

    finished troublshooting speakers page, speakers view was nested in layouts instead of just inside view

--- work/conference/server/routes/speakers/index.js
+++ work/conference/server/routes/speakers/index.js
@@ -4,7 +4,15 @@ const router = express.Router();
 
 module.exports = () => {
   router.get("/", (req, res, next) => {
-    console.log("speakers route");
+    // had to restard the server
+    // return res.send("does this work? Ok now it works");
+
+    // trying to send the pug page does not work
+    // ok i'm getting a 500 status
+    // so it's probably not finding the template
+    // yup! had the directory for speakers inside layouts
+    // it needed to be inside views, if it cannot see the view
+    // it cannot render it - so at least deep nesting is out?
     return res.render("speakers");
   });
 
